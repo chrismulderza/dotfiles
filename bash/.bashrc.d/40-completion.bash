e5379bcdbdbbe42b485206f88506427c1d1a7e48
Chris Mulder <cmulder@bitbuild.io>
2025-09-12 10:26:51 +0200

    Merging MacOS branch

--- bash/.bashrc.d/40-completion.bash
+++ bash/.bashrc.d/40-completion.bash
@@ -1,4 +1,4 @@
-# 40-completion.bash 
+# 40-completion.bash
 # created: 2025-09-11
 # updated: 2025-09-11
 #
@@ -8,10 +8,56 @@
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
 if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-  fi
+    if [ -f /usr/share/bash-completion/bash_completion ]; then
+        . /usr/share/bash-completion/bash_completion
+    elif [ -f /etc/bash_completion ]; then
+        . /etc/bash_completion
+    fi
 fi
 
+# Merge from MacOS branch
+
+# Source bash completions
+#
+#
+# Source the standard bash completions framework
+#test -f /opt/homebrew/etc/bash_completion && source "$_"
+# On MacOS using Homebrew most completions are installed in
+# /opt/homebrew/etc/bash_completion.d/
+
+# homebrew
+#test -f /opt/homebrew/etc/bash_completion.d/brew && source "$_"
+#test -f /opt/homebrew/etc/bash_completion.d/brew-services && source "$_"
+
+# alacritty
+#test -f /opt/homebrew/etc/bash_completion.d/alacritty && source "$_"
+
+# pass
+#test -f /opt/homebrew/etc/bash_completion.d/pass && source "$_"
+
+# pass-otp
+#test -f /opt/homebrew/etc/bash_completion.d/pass-otp && source "$_"
+
+# podman
+#test -f /opt/homebrew/etc/bash_completion.d/podman && source "$_"
+
+# yt-dlp
+#test -f /opt/homebrew/etc/bash_completion.d/yt-dlp && source "$_"
+
+# yt-dlp
+#test -f /opt/homebrew/etc/bash_completion.d/gcalcli && source "$_"
+
+# Custom completions
+# These completions live in ~/dotfiles/bash/completion
+#
+# ilab
+#test -f ~/dotfiles/bash/completion/ilab.bash && source "$_"
+
+# OpenShift Client
+#test -f ~/dotfiles/bash/completion/oc.bash && source "$_"
+
+# Jbang
+#test -f ~/dotfiles/bash/completion/jbang.bash && source "$_"
+
+# Quarkus
+#test -f ~/dotfiles/bash/completion/quarkus.bash && source "$_"
--- bash/.bashrc.d/70-extensions.bash
+++ bash/.bashrc.d/70-extensions.bash
@@ -1,9 +1,20 @@
-# 70-extensions.bash 
+# 70-extensions.bash
 # created: 2025-09-11
 # updated: 2025-09-11
 #
 # direnv
 # TODO - Fix detection. POSIX style
-if which direnv &> /dev/null; then
-  eval "$(direnv hook bash)"
+if which direnv &>/dev/null; then
+    eval "$(direnv hook bash)"
 fi
+
+# Load any shell hooks last
+#
+# direnv
+# eval "$(direnv hook bash)"
+#test -f "$(which direnv)" && eval "$(direnv hook bash)"
+
+# git
+
+# sdkman
+#test -f ~/.sdkman/bin/sdkman-init.sh && source "$_"
